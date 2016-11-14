46927a69f226bdaf84b432d6c7630f8829f35072
Alyssa Tan <alyssamt@uci.edu>
2016-11-14 11:12:20 -0800

    Finalized 'fun_' files

--- CMD-CNTR.gmx/filesystem/TOOLS/virus.py
+++ CMD-CNTR.gmx/filesystem/TOOLS/virus.py
@@ -3,15 +3,26 @@ import os
 import random
 
 
+def walklevel(some_dir, level=1):
+    some_dir = some_dir.rstrip(os.path.sep)
+    assert os.path.isdir(some_dir)
+    num_sep = some_dir.count(os.path.sep)
+    for root, dirs, files in os.walk(some_dir):
+        yield root, dirs, files
+        num_sep_this = root.count(os.path.sep)
+        if num_sep + level <= num_sep_this:
+            del dirs[:]
+
+
 if __name__ == "__main__":
 	num = int(sys.argv[1])
 
 	l = []
-	for root, directories, filenames in os.walk('../ARCHITECTURE'):
+	for root, directories, filenames in walklevel('../ARCHITECTURE'):
 		for filename in filenames: 
 			l.append(os.path.join(root,filename))
 
-	for root, directories, filenames in os.walk('../SECURITY'):
+	for root, directories, filenames in walklevel('../SECURITY'):
 		for filename in filenames: 
 			l.append(os.path.join(root,filename))
 
