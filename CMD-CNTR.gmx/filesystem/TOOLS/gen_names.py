dce3baf3ad24280df16e22c781ad38af97329572
Alyssa Tan <alyssamt@uci.edu>
2016-11-14 17:41:09 -0800

    Removed filesystem, added paths.txt

--- CMD-CNTR.gmx/filesystem/TOOLS/gen_names.py
+++
@@ -1,7 +0,0 @@
-import os
-import random
-from itertools import izip
-
-if __name__ == "__main__":
-	for first, last in izip(open("first.txt"), open("last.txt")):
-		os.makedirs("../PERSON_DATABASE/{}".format('_'.join([first.split()[0], last.split()[0], str(random.randrange(0,1000))])))
